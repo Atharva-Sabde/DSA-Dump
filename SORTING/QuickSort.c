a27c2a9f69d81fc9a4cc1e1801bbf2df1cedf84e
Atharva Sabde <atharvaa.sabde@gmail.com>
2021-09-05 21:54:17 +0530

    bug bug

--- SORTING/QuickSort.c
+++ SORTING/QuickSort.c
@@ -1,33 +1,51 @@
 #include<stdio.h>
-   
-
+void swap(int* a, int* b) { 
+    int t = *a; 
+    *a = *b; 
+    *b = t; 
+} 
+  
 int partition(int list[], int lb , int ub){
     int pivot  , i , j ;
     pivot = list[lb];
-    i=l;
-    j=h;
+    i=lb;
+    j=ub;
 
-    while(i<j){
-        do{i++}while(list[i]<=pivot)
-        do{j--}while(list[j]>pivot)
+    do{
+        do{i++;}while(list[i]<=pivot);
+        do{j--;}while(list[j]>pivot);
         if(i>j){
-            list[i]=list[i]+list[j];
-            list[j]=list[i]-list[j];
-            list[i]=list[i]-list[j];
+            // list[i]=list[i]+list[j];
+            // list[j]=list[i]-list[j];
+            // list[i]=list[i]-list[j];
+            swap(&list[i],&list[j]);
         }
-    }
-    pivot = pivot + list[j];
-    list[j]= pivot - list[j];
-    pivot = pivot -jist[j];
-
+    }while(i<j);
+    // pivot = pivot + list[j];
+    // list[j]= pivot - list[j];
+    // pivot = pivot -list[j];
+    swap(&pivot,&list[j]);
+ 
     return j;
 }
-int 
+
+void quickSort(int arr[] , int lb , int ub){
+    int j;
+    if(lb<ub){
+    j=partition(arr , lb , ub);
+    quickSort(arr,lb,j-1);
+    quickSort(arr,j+1 , ub);
+    }
+} 
 int main() {
+    int i;
+    int arr[6]= {7,4,5,2,7,1};
 
-    int arr[6]= {'4','1','5','9','3','8'};
+    quickSort(arr , 0 , 5);
 
-    partition(arr , 0 , 5);
-
+    for(i=0 ; i<6 ; i++){
+        printf("%d ",arr[i]);
+    }
     return 0;
 }
+   
